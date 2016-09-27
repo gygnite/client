bf550a2a96d36b0b862ec3c03ae9570844359fcb
Michael Hottman <hottmanmichael@gmail.com>
2016-09-27 12:09:24 -0600

    added photo upload to bands and venues

--- scripts/components/controllers/dashboard/bands/new.js
+++ scripts/components/controllers/dashboard/bands/new.js
@@ -6,6 +6,8 @@ var request = require('superagent');
 require('superagent-auth-bearer');
 var Cache = require('lscache');
 var browserHistory = require('react-router').browserHistory;
+const CLOUDINARY_UPLOAD_PRESET = 'gygnite';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/gygnite/upload';
 
 var connect = require('react-redux').connect;
 
@@ -15,9 +17,7 @@ function mapStateToProps(state, ownProps) {
 
 function mapDispatchToProps(dispatch) {
     return {
-        createBand: function(band) {
-
-            // console.log("band.profileimage", band.profile_image);
+        createBand: function(band, image) {
 
             dispatch(ACTIONS.ui.fetchBands());
 
@@ -28,11 +28,12 @@ function mapDispatchToProps(dispatch) {
                     console.log("log err", err, res.body);
 
                     if (!err && !res.body.error) {
-                        dispatch(ACTIONS.ui.createAlert(res.body.data.band.name + ' created successfully!', 'success'));
-                        browserHistory.replace('/dashboard');
+                        if (image) {
+                            saveToCloud(image, res.body.data.band.id);
+                        } else {
+                            finish(res.body.data.band);
+                        }
                     } else{
-                        //error
-                        // FIXME: New Band Error
                         var message = 'Unable to create band.';
                         if (res.body.message) {
                             message = res.body.message;
@@ -41,6 +42,33 @@ function mapDispatchToProps(dispatch) {
                     }
                     dispatch(ACTIONS.ui.fetchBandsComplete());
                 });
+
+            function saveToCloud(image, id) {
+                request.post(CLOUDINARY_UPLOAD_URL)
+                    .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+                    .field('file', image)
+                    .end(function(err, res) {
+                        // FIXME: handle cloudinary upload error
+                        addImageToBand(res.body.url, id);
+                    });
+            }
+
+            function addImageToBand(image, id) {
+                request.post(BASE_URL+'/api/admins/bands/image')
+                    .send({
+                        image_url: image,
+                        id: id
+                    })
+                    .authBearer(Cache.get(ACTIONS.cache.AUTH_TOKEN))
+                    .end(function(err, res) {
+                        finish(res.body.band);
+                    });
+            }
+
+            function finish(band) {
+                dispatch(ACTIONS.ui.createAlert(band.name + ' created successfully!', 'success'));
+                browserHistory.replace('/dashboard');
+            }
         }
     }
 }
